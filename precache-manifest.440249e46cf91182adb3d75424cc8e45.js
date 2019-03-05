dc5a535888587c8865954eefe4c2f631fd33d678
Barklim <kliment.barkalov@gmail.com>
2019-03-05 05:07:07 +0300

    Updates

---
+++ precache-manifest.440249e46cf91182adb3d75424cc8e45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aa811f4b17297582e90",
+    "url": "/wines/static/js/runtime~main.9aa811f4.js"
+  },
+  {
+    "revision": "77801a0a93a77437737b",
+    "url": "/wines/static/js/main.77801a0a.chunk.js"
+  },
+  {
+    "revision": "00ec0959a47ed983999e",
+    "url": "/wines/static/js/2.00ec0959.chunk.js"
+  },
+  {
+    "revision": "77801a0a93a77437737b",
+    "url": "/wines/static/css/main.1ae5761a.chunk.css"
+  },
+  {
+    "revision": "8e9033d8b1446340579d09f19fdb6b1b",
+    "url": "/wines/index.html"
+  }
+];
